29470df874aa3a39e9f3c5e542ec1cd30c1e5f9e
georgemhall <georgemhall@users.noreply.github.com>
2018-09-25 22:47:28 -0400

    Fixed algorithm and cleaned up code

--- MonteCarloPi.py
+++ MonteCarloPi.py
@@ -1,6 +1,7 @@
+# A multi-threaded monte carlo simulation for computing pi
 import threading, time, math, random, Queue
 
-def PiSolver(numThrows, points_queue):
+def PiSolver(numThrows, hits_queue):
     circle_points = 0
     for x in range(0, numThrows, 1):
         randX = (random.random() * 2) - 1
@@ -8,17 +9,19 @@ def PiSolver(numThrows, points_queue):
         distance = math.hypot(randX, randY)
         if(distance < 1):
             circle_points += 1
-    points_queue.put(circle_points)
+    hits_queue.put(circle_points)
 
 if __name__ == '__main__':
-    my_queue = Queue.Queue()
-    total_throws = 10000000
-    ret = 0
+    result_queue = Queue.Queue()
+    hits = 0
+    total_throws = 100000
     num_threads = 4
+    throws_per_thread = total_throws / num_threads
+
     for x in range(num_threads):
-        mythread = threading.Thread(PiSolver(total_throws, my_queue))
+        # Create a new thread targeting the PiSolver method and a queue for storing results
+        mythread = threading.Thread(PiSolver(throws_per_thread, result_queue))
         mythread.start()
         mythread.join()
-        ret += my_queue.get()
-        time.sleep(.9)
-    print (4.0*ret)/(num_threads*total_throws)
+        hits += result_queue.get()
+    print 4.0*hits/(num_threads*throws_per_thread)
